d310445961c6a405dd0a7809b63e7a1f4dc66780
Ezequiel Pereira <ezequielpereira92@gmail.com>
2022-06-04 18:18:29 -0300

    Obtener y mostrar datos del usuario con swing

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 /**
  * 
  */
@@ -12,8 +14,30 @@ public class App {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Object[] opciones = { "Pesos a Dólares", "Dólares a pesos" };
+
+		String opcionElegida = (String) JOptionPane.showInputDialog(null, "Seleccione una opción", "OPCIONES",
+				JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+		double valorConvertir = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor a convertir"));
+
+		switch (opcionElegida) {
+		case "Pesos a Dólares":
+			valorConvertir = valorConvertir / 200;
+			break;
+
+		case "Dólares a pesos":
+			valorConvertir = valorConvertir * 200;
+			break;
+
+		default:
+			break;
+		}
+
+		String mensaje = "El valor convertido es: " + valorConvertir;
+
+		JOptionPane.showMessageDialog(null, mensaje);
 	}
 
 }
